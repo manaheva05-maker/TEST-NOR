031a68b622aca5597d9041c67c3add92e76354bf
hostinginconnu-boop <hostinginconnu@gmail.com>
2026-06-05 19:46:57 -0300

    INCONNU BOY TECH 🍂

--- inconnuboy/core/context.js
+++ inconnuboy/core/context.js
@@ -1,7 +1,7 @@
 const { getSetting } = require("./database/settings");
 
 async function getContextInfo(mentionedJid = []) {
-    const botName = await getSetting("BOT_NAME") || "𝐀𝐓𝐀𝐒𝐒𝐀-𝐌𝐃";
+    const botName = await getSetting("BOT_NAME") || "INCONNU XD V3";
     const channelJid = await getSetting("NEWSLETTER_JID") || "120363403408693274@newsletter";
     return {
         mentionedJid,
