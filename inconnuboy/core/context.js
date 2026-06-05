914eded7297cd8bc3721ce92ada48606c34fc766
hostinginconnu-boop <hostinginconnu@gmail.com>
2026-06-05 18:43:30 -0300

    Upload files - BY TEDDY-XMD

---
+++ inconnuboy/core/context.js
@@ -0,0 +1,18 @@
+const { getSetting } = require("./database/settings");
+
+async function getContextInfo(mentionedJid = []) {
+    const botName = await getSetting("BOT_NAME") || "𝐀𝐓𝐀𝐒𝐒𝐀-𝐌𝐃";
+    const channelJid = await getSetting("NEWSLETTER_JID") || "120363403408693274@newsletter";
+    return {
+        mentionedJid,
+        forwardingScore: 1,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: channelJid,
+            newsletterName: botName,
+            serverMessageId: -1
+        }
+    };
+}
+
+module.exports = { getContextInfo };
