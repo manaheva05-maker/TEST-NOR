f55877b14d324a74ec59e44be3e50a3e80a8f664
hostinginconnu-boop <hostinginconnu@gmail.com>
2026-06-05 20:10:46 -0300

    INCONNU BOY TECH 🍂

--- inconnuboy/core/helpers.js
+++ inconnuboy/core/helpers.js
@@ -1,4 +1,4 @@
-const { getSetting } = require("./database/settings");
+const { getSetting } = require("./store/settings");
 
 const originalConsoleInfo = console.info;
 const originalConsoleLog = console.log;
